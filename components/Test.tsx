ed9887ae570f9b3a5765ca882a7587abbaa738d2
Tiến Đạt <ntd.k108cva@gmail.com>
2022-12-24 14:55:52 +0700

    update

---
+++ components/Test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1>{`Test`}</h1>
+    </div>
+  );
+};
+
+export default Test;
---
+++ pages/test/index.tsx
@@ -0,0 +1,11 @@
+import Test from "../../components/Test";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+};
+
+export default IndexPage;
